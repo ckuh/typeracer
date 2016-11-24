71eac1fa14cb435386ae75790bd6784473566a61
ckuh <kuh_chris@yahoo.com>
2016-11-23 16:19:15 -0800

    [feature] project init

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.set('port', process.env.PORT || 1337)
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
+
+app.listen(app.get('port'), () => {
+  console.log('Server started listening to port ', app.get('port'))
+})
